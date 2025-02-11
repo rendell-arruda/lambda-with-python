e88d1a63c5aa8308f4586f662166e8c26b454f06
Rendell Arruda <rendellarruda3@gmail.com>
2025-02-10 21:29:11 -0300

    add teste app

--- scheduled/app.py
+++ scheduled/app.py
@@ -3,7 +3,7 @@ from chalice import Chalice
 app = Chalice(app_name='scheduled')
 
 
-@app.schedule("cron(25 21 ? * * *)")
+@app.schedule("cron(30 21 ? * * *)")
 def scheduled(event):
     print("I'm running every day at 9:30pm")
     return True
