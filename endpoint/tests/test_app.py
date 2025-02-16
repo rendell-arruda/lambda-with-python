0491b762f40a0802aa08e4a8d0c39d1aca45a784
Rendell Arruda <rendellarruda3@gmail.com>
2025-02-16 17:53:07 -0300

    feat endpoint

--- endpoint/tests/test_app.py
+++ endpoint/tests/test_app.py
@@ -4,5 +4,5 @@ from app import app
 
 def test_index():
     with Client(app) as client:
-        response = client.http.get('/')
-        assert response.json_body == {'hello': 'world'}
+        response = client.http.get('/id/123')
+        assert response.json_body == True
